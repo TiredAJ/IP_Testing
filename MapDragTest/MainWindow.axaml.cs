d5d1aca48f9e9645881af1ae14812a61067b5304
AJ <aletha.e.jones@gmail.com>
2023-10-23 14:21:18 +0100

    init 2

--- MapDragTest/MainWindow.axaml.cs
+++ MapDragTest/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using System.IO;
 
 namespace MapDragTest
 {
@@ -7,6 +10,16 @@ namespace MapDragTest
         public MainWindow()
         {
             InitializeComponent();
+
+            //GenerateImage(Properties.Resources.Fox., img_Main);
+
+            img_Main.Source = new Bitmap(Properties.Resources.Fox);
+        }
+
+        public void GenerateImage(byte[] _Data, Image _Ctrl)
+        {
+            using (Stream S = new MemoryStream(_Data))
+            { _Ctrl.Source = new Bitmap(S); }
         }
     }
 }
